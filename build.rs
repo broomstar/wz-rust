1a5e9c35ce2461625d412c209d3235c9352404e8
unknown <broomstar@163.com>
2021-02-02 22:17:31 +0800

    use cc instead of cmake

--- build.rs
+++ build.rs
@@ -1,16 +1,29 @@
 extern crate bindgen;
-extern crate cmake;
 
 use std::env;
 use std::path::{Path, PathBuf};
 use std::process::Command;
 
 fn main() {
-    if !Path::new("libwz/.git").exists() {
+    if !Path::new("wz/.git").exists() {
         let _ = Command::new("git")
             .args(&["submodule", "update", "--init"])
             .status();
     }
+
+    let mut cfg = cc::Build::new();
+
+    cfg.file("libwz/src/lib/aes256.c")
+        .file("libwz/src/byteorder.c")
+        .file("libwz/src/file.c")
+        .include("libwz/src")
+        .include("libwz/src/lib");
+
+    if let Some(include) = std::env::var_os("DEP_Z_INCLUDE") {
+        cfg.include(include);
+    }
+
+    cfg.compile("wz");
 
     let bindings = bindgen::Builder::default()
         .header("libwz/src/wz.h")
@@ -21,8 +34,4 @@ fn main() {
     bindings
         .write_to_file(out_path.join("bindings.rs"))
         .expect("Couldn't write bindings!");
-
-    let dst = cmake::Config::new("libwz").cflag("-Wno-comma").build();
-    println!("cargo:rustc-link-search=native={}/lib", dst.display());
-    println!("cargo:rustc-link-lib=dylib=wz");
 }
